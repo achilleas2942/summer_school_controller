55b30d526746dd79ef148cc4c031d5898685c045
Achilleas Seisa <achilleas2942@gmail.com>
2024-06-03 18:18:16 +0200

    UDP tunnels

--- src/position_prediction.py
+++ src/position_prediction.py
@@ -8,8 +8,8 @@ from geometry_msgs.msg import TwistStamped
 
 class PositionPredictor:
     def __init__(self):
-        self.delay = 0.0
-        self.dt = 0.0
+        self.uplink = 0.0
+        self.downlink = 0.0
         self.counter = 0
         self.cmd_vel = np.array([0.0, 0.0, 0.0])
 
@@ -25,25 +25,30 @@ class PositionPredictor:
         self.estimated_odometry = Odometry()
         recv_time = rospy.Time.now()
         pub_time = msg.header.stamp
-        self.delay = (recv_time - pub_time).to_sec()
-        self.delay = 0
-        if self.counter < 500:  # sliding window of ~5s
+        delay = (recv_time - pub_time).to_sec()
+        if self.counter < 500:  # sliding window of 500 messages
             self.counter = self.counter + 1
-        self.dt = np.divide(self.delay + (self.counter - 1) * self.dt, self.counter)
+        self.uplink = np.divide(delay + (self.counter - 1) * self.uplink, self.counter)
         self.estimated_odometry = msg
         self.estimated_odometry.pose.pose.position.x = (
-            msg.pose.pose.position.x + self.cmd_vel[0] * self.dt
+            msg.pose.pose.position.x + self.cmd_vel[0] * (self.uplink + self.downlink)
         )
         self.estimated_odometry.pose.pose.position.y = (
-            msg.pose.pose.position.y + self.cmd_vel[1] * self.dt
+            msg.pose.pose.position.y + self.cmd_vel[1] * (self.uplink + self.downlink)
         )
         self.estimated_odometry.pose.pose.position.z = (
-            msg.pose.pose.position.z + self.cmd_vel[2] * self.dt
+            msg.pose.pose.position.z + self.cmd_vel[2] * (self.uplink + self.downlink)
         )
         self.estimated_odometry.header.stamp = rospy.Time.now()
         self.est_odom_pub.publish(self.estimated_odometry)
 
     def callback_cmd_vel(self, msg):
+        recv_time = rospy.Time.now()
+        pub_time = msg.header.stamp
+        delay = (recv_time - pub_time).to_sec()
+        if self.counter < 500:  # sliding window of 500 messages
+            self.counter = self.counter + 1
+        self.downlink = np.divide(delay + (self.counter - 1) * self.downlink, self.counter)
         self.cmd_vel[0] = msg.twist.linear.x
         self.cmd_vel[1] = msg.twist.linear.y
         self.cmd_vel[2] = msg.twist.linear.z
